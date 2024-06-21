097d65a44529ece6db4f0ef83b0501ca8e7a2838
tanmay <154451032+tanmaydev56@users.noreply.github.com>
2024-06-21 17:36:05 +0530

    Update ComputersCanvas.jsx

--- src/components/canvas/ComputersCanvas.jsx
+++ src/components/canvas/ComputersCanvas.jsx
@@ -26,7 +26,7 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={gltf.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.5 : 0.65}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
